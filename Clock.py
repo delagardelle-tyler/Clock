b9c9d7448ae7834d09fdd5fe430ea3c1f8586098
delagardelle-tyler <delagardelle.tyler@gmail.com>
2025-01-19 15:07:00 -0600

    Clock.py

--- Clock.py
+++ Clock.py
@@ -8,16 +8,19 @@ from tkinter import Tk
 from tkinter import Label
 import time
 import sys
-
+#opens base window
 window = Tk()
 window.title("Digital clock")
-
+#clock title
 clock = Label(window , font = ("calibri" , 90) , bg = "#966659" , fg = "white")
 clock.pack()
 
 def updateClock():
+    #collects current time
     timeVar = time.strftime("%I:%M:%S %p")
+    #changes text to current time
     clock.config(text=timeVar)
+    #after 1 seconds, update clock
     clock.after(1000,updateClock)
 
 updateClock()
